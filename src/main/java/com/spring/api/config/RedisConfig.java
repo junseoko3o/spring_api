b346f2204551768550f36bb077a2741022e09e0c
junseoko3o <github.yam117@passinbox.com>
2024-12-28 01:53:00 +0900

    Fix: remove sessionrepository customizer

--- src/main/java/com/spring/api/config/RedisConfig.java
+++ src/main/java/com/spring/api/config/RedisConfig.java
@@ -9,14 +9,7 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import org.springframework.session.MapSession;
-import org.springframework.session.config.SessionRepositoryCustomizer;
-import org.springframework.session.data.redis.RedisSessionMapper;
-import org.springframework.session.data.redis.RedisSessionRepository;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
-
-import java.util.Map;
-import java.util.function.BiFunction;
 
 @Configuration
 @EnableRedisHttpSession
@@ -50,31 +43,31 @@ public class RedisConfig {
         return redisTemplate;
     }
 
-    @Bean
-    SessionRepositoryCustomizer<RedisSessionRepository> redisSessionRepositoryCustomizer() {
-        return (redisSessionRepository) -> redisSessionRepository
-                .setRedisSessionMapper(new SafeRedisSessionMapper(redisSessionRepository));
-    }
-
-    static class SafeRedisSessionMapper implements BiFunction<String, Map<String, Object>, MapSession> {
-
-        private final RedisSessionMapper delegate = new RedisSessionMapper();
-
-        private final RedisSessionRepository sessionRepository;
-
-        SafeRedisSessionMapper(RedisSessionRepository sessionRepository) {
-            this.sessionRepository = sessionRepository;
-        }
-
-        @Override
-        public MapSession apply(String sessionId, Map<String, Object> map) {
-            try {
-                return this.delegate.apply(sessionId, map);
-            }
-            catch (IllegalStateException ex) {
-                this.sessionRepository.deleteById(sessionId);
-                return null;
-            }
-        }
-    }
+//    @Bean
+//    SessionRepositoryCustomizer<RedisSessionRepository> redisSessionRepositoryCustomizer() {
+//        return (redisSessionRepository) -> redisSessionRepository
+//                .setRedisSessionMapper(new SafeRedisSessionMapper(redisSessionRepository));
+//    }
+//
+//    static class SafeRedisSessionMapper implements BiFunction<String, Map<String, Object>, MapSession> {
+//
+//        private final RedisSessionMapper delegate = new RedisSessionMapper();
+//
+//        private final RedisSessionRepository sessionRepository;
+//
+//        SafeRedisSessionMapper(RedisSessionRepository sessionRepository) {
+//            this.sessionRepository = sessionRepository;
+//        }
+//
+//        @Override
+//        public MapSession apply(String sessionId, Map<String, Object> map) {
+//            try {
+//                return this.delegate.apply(sessionId, map);
+//            }
+//            catch (IllegalStateException ex) {
+//                this.sessionRepository.deleteById(sessionId);
+//                return null;
+//            }
+//        }
+//    }
 }
