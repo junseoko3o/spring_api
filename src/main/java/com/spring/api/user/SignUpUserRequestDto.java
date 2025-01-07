97faec8daa01d5bf5d72fff4c37474abd561abcc
junseoko3o <github.yam117@passinbox.com>
2025-01-07 23:14:35 +0900

    Feat: 회원가입시 양식 추가

--- src/main/java/com/spring/api/user/SignUpUserRequestDto.java
+++ src/main/java/com/spring/api/user/SignUpUserRequestDto.java
@@ -3,16 +3,20 @@ package com.spring.api.user;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class SignUpUserRequestDto {
-    @Email
+    @Email(message = "email 형식 아님")
     @NotNull
     @NotBlank
     private String email;
 
     @NotNull
     @NotBlank
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[~!@#$%^&*()+|=])[A-Za-z\\d~!@#$%^&*()+|=]{8,16}$",
+            message = "비밀번호는 숫자, 문자, 특수문자 1개 이상 ")
     private String password;
 }
